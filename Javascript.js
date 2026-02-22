2dc93a715d4c91bba53ef3ad6656e3852ccbb87c
Vuthsophany <nnap1024@gmail.com>
2026-02-22 15:40:34 +0700

    put icon on sidebar

--- Javascript.js
+++ Javascript.js
@@ -20,7 +20,7 @@ document.getElementById("show-more-btn").addEventListener("click", function(e) {
     this.style.display = "none";
 });
 
-/*                                                                          when click on three bar */
+
 const menuIcon = document.querySelector(".fa-bars");
 const sidebar = document.querySelector(".sidebar");
 
@@ -28,10 +28,40 @@ menuIcon.addEventListener("click", function(){
     sidebar.classList.toggle("active");
 });
              
+// This listens for clicks anywhere on the window
+window.addEventListener('click', function(e) {
+    
+    if (sidebar.classList.contains('active')) {
+        // If the click is not the sidebar and not the menu icon
+        if (!sidebar.contains(e.target) && !menuIcon.contains(e.target)) {
+            sidebar.classList.remove('active');
+        }
+    }
+});
 
 
 
 
+/*ah nis hav from html*/ 
+const registerBtn = document.querySelector('.register-button');
+const regPopup = document.getElementById('regPopup');
+const closeBtn = document.querySelector('.exit-popup');
 
+// When clicking Register, add the 'open' class
+registerBtn.addEventListener('click', () => {
+    regPopup.classList.add('open');
+});
+
+// When clicking the 'X', remove the 'open' class
+closeBtn.addEventListener('click', () => {
+    regPopup.classList.remove('open');
+});
+
+// Optional: Close if clicking on the dark background area
+window.addEventListener('click', (e) => {
+    if (e.target === regPopup) {
+        regPopup.classList.remove('open');
+    }
+});
 
 
