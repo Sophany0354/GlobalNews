e4088e16e545d8f786f7eb116c990729ae117523
Vuthsophany <nnap1024@gmail.com>
2026-03-31 14:19:11 +0700

    add technology and health

--- Javascript.js
+++ Javascript.js
@@ -324,3 +324,10 @@ document.addEventListener("DOMContentLoaded", () => {
     }, { threshold: 0.1, rootMargin: "0px 0px -50px 0px" });
     cards.forEach(card => observer.observe(card));
 });
+
+// bos technology
+
+  AOS.init({
+    once: false, 
+    mirror: true, 
+  });
